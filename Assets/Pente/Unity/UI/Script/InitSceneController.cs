fe7b6aabe2b85b0588d9d079f4824a79f1fb6686
Chris Hanna <cdhannaphone@gmail.com>
2021-04-07 13:39:31 -0400

    working on inv

--- Assets/Pente/Unity/UI/Script/InitSceneController.cs
+++ Assets/Pente/Unity/UI/Script/InitSceneController.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InitSceneController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Beamable.API.Instance.Then(_ =>
+        {
+            var load = SceneManager.LoadSceneAsync("Lobby");
+            load.allowSceneActivation = true;
+            // TODO: set up loading animation?
+        });
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ Assets/Pente/Unity/UI/Script/InventoryController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryController : MonoBehaviour
+{
+    public Canvas MainCanvas;
+    public Button TriggerButton;
+
+    public Button CloseButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TriggerButton.onClick.AddListener(Toggle);
+        CloseButton.onClick.AddListener(Close);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Close()
+    {
+        MainCanvas.gameObject.SetActive(false);
+    }
+    public void Toggle()
+    {
+        MainCanvas.gameObject.SetActive(!MainCanvas.gameObject.activeInHierarchy);
+    }
+}
