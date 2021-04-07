7643f91934877f1232f7a72c1e44e1eaa8f8c7ce
Chris Hanna <cdhannaphone@gmail.com>
2021-04-07 12:00:57 -0400

    simple menu hooksups

---
+++ Assets/Pente/Unity/UI/Script/LobbySceneController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbySceneController : MonoBehaviour
+{
+    public Button startButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        // transition the scene to the boardDemo scene...
+        var load = SceneManager.LoadSceneAsync("BoardDemo");
+        startButton.enabled = false;
+        load.allowSceneActivation = true;
+
+        // do some sort of loading animation?
+    }
+}
